6dee9f67e985e1793903e6dfdb4ff49da23044e9
张哲铭 <1371955569@qq.com>
2017-12-03 10:44:25 +0800

    fix bug

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './main.css'
 import FileTableContainer from '../containers/FileTableContainer'
 import DirMenuContainer from '../containers/DirMenuContainer'
-import UploaderContainer from '../components/UploaderContainer.js'
+import UploaderContainer from '../containers/UploaderContainer'
 import styled from 'styled-components'
 import { Layout } from 'antd'
 const { Header, Sider, Content } = Layout
