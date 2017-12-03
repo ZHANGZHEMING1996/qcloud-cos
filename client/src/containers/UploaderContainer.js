1a46e4eaefa95c75a34347bbb97204058566dced
张哲铭 <1371955569@qq.com>
2017-12-03 10:59:17 +0800

    文件上传到 Buckket，实现与腾讯云的鉴权

--- client/src/containers/UploaderContainer.js
+++ client/src/containers/UploaderContainer.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import Settings from '../settings'
+import cos from '../lib/qcloud'
 
 class UploaderContainer extends Component {
   handleChange = (e) => {
     const file = e.target.files[0]
-    console.log('UploaderContainer', file)
+    const params = {
+      Bucket: Settings.Bucket,
+      Region: Settings.Region,
+      Key: '/aa/zhang.txt',
+      Body: file
+    }
+    cos.sliceUploadFile(params, (err, data) => {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log(data)
+      }
+    })
   }
 
   render () {
